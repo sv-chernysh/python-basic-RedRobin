bd7c568a0cd012cc2e38e4445d2d4e231606fdfa
Serhii Chernysh <srg.chernysh@gmail.com>
2022-10-20 22:09:29 +0300

    task: # 2. Append 4 and 5 to the lst_d and define the id one more time - Done

--- lecture-3-hw/main.py
+++ lecture-3-hw/main.py
@@ -6,3 +6,8 @@ lst_d = [1, 2, 3]
 dict_e = {'a': 1, 'b': 2, 'c': 3}
 
 print(id(int_a), id(str_b), id(set_c), id(lst_d), id(dict_e))
+
+# 2. Append 4 and 5 to the lst_d and define the id one more time.
+lst_d.append(4)
+lst_d.append(5)
+print(id(lst_d))
