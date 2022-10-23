53b800f79dad2e7a7a1d30482cdfd51a36f7f767
Serhii Chernysh <srg.chernysh@gmail.com>
2022-10-24 01:30:07 +0300

    task from 18 to 26 done

--- lecture-3-hw/main.py
+++ lecture-3-hw/main.py
@@ -121,3 +121,54 @@ for x in range(10):
     else:
         dict_2[x] = x
 print(dict_2)
+
+# Lambda:
+
+# (7)
+
+# def foo(x, y):
+#     if x < y:
+#         return x
+#     else:
+#         return y
+
+# (8)
+# foo = lambda x, y, z: z if y < x and x > z else y
+
+# 18. Convert (7) to lambda function
+foo_1 = lambda x, y: x if (x < y) else y
+
+
+# 19*. Convert (8) to regular function
+def foo_2(x, y, z):
+    if y < x and x > z:
+        return z
+    else:
+        return y
+
+
+lst_to_sort = [5, 18, 1, 24, 33, 15, 13, 55]
+# 20. Sort lst_to_sort from min to max
+print(sorted(lst_to_sort))
+
+# 21. Sort lst_to_sort from max to min
+print(sorted(lst_to_sort, reverse=True))
+
+# 22. Use map and lambda to update the lst_to_sort by multiply each element by 2
+print(list(map(lambda x: x * 2, lst_to_sort)))
+
+# 23*. Raise each list number to the corresponding number on another list:
+list_A = [2, 3, 4]
+list_B = [5, 6, 7]
+print(list(map(lambda x, y: x + y, list_A, list_B)))
+
+# 24. Use filter and lambda to filter the number of a lst_to_sort with elem % 2 == 1.
+print(list(filter(lambda x: x % 2 == 1, lst_to_sort)))
+
+# 25. Considering the range of values: b = range(-10, 10), use the function filter to return only negative numbers.
+print(list(filter(lambda x: x < 0, range(-10, 10))))
+
+# 26*. Using the filter function, find the values that are common to the two lists:
+list_1 = [1, 2, 3, 5, 7, 9]
+list_2 = [2, 3, 5, 6, 7, 8]
+print(list(filter(lambda x: x in list_1, list_2)))
