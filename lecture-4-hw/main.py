8efb1d22dfd80c18769c4d92f32290c046d64884
Serhii Chernysh <srg.chernysh@gmail.com>
2022-10-29 20:14:52 +0300

    task from 1 to 3 done

--- lecture-4-hw/main.py
+++ lecture-4-hw/main.py
@@ -1,5 +1,5 @@
 class Vehicle:
-    def __int__(self, max_speed, mileage):
+    def __init__(self, max_speed, mileage):
         self.max_speed = max_speed
         self.mileage = mileage
 
@@ -11,3 +11,23 @@ class Vehicle:
 
     def mileage_info(self):
         print("Mileage:", self.mileage)
+
+
+class Bus(Vehicle):
+    def __init__(self, max_speed, mileage, seating_capacity_info):
+        super().__init__(max_speed, mileage)
+        self.seating_capacity_info = seating_capacity_info
+
+    def seating_capacity(self):
+        print(f'Seating capacity: {self.seating_capacity_info} {self.__class__.__name__}')
+
+
+print(issubclass(Bus, Vehicle))
+# ford = Vehicle(120, 8000)
+# ford.increase_speed()
+# ford.break_speed()
+# ford.mileage_info()
+#
+# bohdan = Bus(100, 2000, 30)
+# bohdan.mileage_info()
+# bohdan.seating_capacity()
