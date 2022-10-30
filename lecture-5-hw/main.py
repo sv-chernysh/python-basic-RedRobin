da708d2cfb1dd320c9188c263a2f8f8a318fdad5
Serhii Chernysh <srg.chernysh@gmail.com>
2022-10-30 19:30:45 +0200

    lecture-5 task from 8 to 10 done

--- lecture-5-hw/main.py
+++ lecture-5-hw/main.py
@@ -130,11 +130,8 @@ concert.visitors_count = 1000
 print(concert.visitors_count)
 
 
-# 6.
-# class AddressBookDataClass:
-#     """
-#     Create dataclass with 7 fields - key (int), name (str), phone_number (str), address (str), email (str), birthday (str), age (int)
-#     """
+# 6. class AddressBookDataClass: """ Create dataclass with 7 fields - key (int), name (str), phone_number (str),
+# address (str), email (str), birthday (str), age (int) """
 @dataclasses.dataclass
 class AddressBookDataClass:
     key: int
@@ -149,3 +146,75 @@ class AddressBookDataClass:
 # 7. Create the same class (6) but using NamedTuple
 AddressBookDataClass = collections.namedtuple('AddressBookDataClass', ['key', 'name', 'phone_number', 'address',
                                                                        'email', 'birthday', 'age'])
+
+
+# 8.
+# class AddressBook:
+#     """
+#     Create regular class taking 7 params on init - key, name, phone_number, address, email, birthday, age
+#     Make its str() representation the same as for AddressBookDataClass defined above.
+#     Expected result by printing instance of AddressBook: AddressBook(key='', name='', phone_number='', address='', email='', birthday= '', age='')
+#     """
+class AddressBook:
+    def __init__(self, key, name, phone_number, address, email, birthday, age):
+        self.key = key
+        self.name = name
+        self.phone_number = phone_number
+        self.address = address
+        self.email = email
+        self.birthday = birthday
+        self.age = age
+
+    def __str__(self):
+        return f'{self.__class__.__name__}(key={self.key}, name={self.name}, phone_number={self.phone_number}, ' \
+               f'address={self.address}, email={self.email}, birthday={self.birthday}, age={self.age})'
+
+
+print(AddressBook(15, 'Serhii', '+380960000000', 'Zhytomyr', 'some@gmail.com', '20.01.1990', 32))
+
+# 9.
+# class Person:
+#     """
+#     Change the value of the age property of the person object
+#     """
+#     name = "John"
+#     age = 36
+#     country = "USA"
+9.
+
+
+class Person:
+    name = "John"
+    age = 36
+    country = "USA"
+
+
+Person.age = 77
+print(Person.age)
+
+
+# 10.
+# class Student:
+#     """
+#     Add an 'email' attribute of the object student and set its value
+#     Assign the new attribute to 'student_email' variable and print it by using getattr
+#     """
+#     id = 0
+#     name = ""
+#
+#     def __init__(self, id, name):
+#         self.id = id
+#         self.name = name
+class Student:
+    id = 0
+    name = ""
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+student = Student(7, 'Robert')
+setattr(student, 'email', 'some@gmail.com')
+student_email = getattr(student, 'email')
+print(student_email)
